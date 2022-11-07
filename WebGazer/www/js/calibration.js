336dc20fc36a4a76644b57bdc98a8634bd17f359
ponciks <mehmeteminselek@gmail.com>
2022-11-07 23:06:48 +0300

    square

--- WebGazer/www/js/calibration.js
+++ WebGazer/www/js/calibration.js
@@ -16,8 +16,8 @@ function ClearCanvas(){
 function PopUpInstruction(){
   ClearCanvas();
   swal({
-    title:"Calibration",
-    text: "Please click on each of the 9 points on the screen. You must click on each point 5 times till it goes yellow. This will calibrate your eye movements.",
+    title:"Calibrazione",
+    text: "Fare clic su ciascuno dei 9 punti sullo schermo. Devi fare clic su ogni punto 5 volte finché non diventa giallo. Questo calibrerà i tuoi movimenti oculari.",
     buttons:{
       cancel: false,
       confirm: true
@@ -77,8 +77,8 @@ $(document).ready(function(){
 
             // notification for the measurement process
             swal({
-              title: "Calculating measurement",
-              text: "Please don't move your mouse & stare at the middle dot for the next 5 seconds. This will allow us to calculate the accuracy of our predictions.",
+              title: "Calcolo della misura",
+              text: "Per favore non muovere il mouse e fissare il punto centrale per i prossimi 5 secondi. Questo ci permetterà di calcolare l'accuratezza delle nostre previsioni.",
               closeOnEsc: false,
               allowOutsideClick: false,
               closeModal: true
@@ -93,17 +93,17 @@ $(document).ready(function(){
                       stop_storing_points_variable(); // stop storing the prediction points
                       var past50 = webgazer.getStoredPoints(); // retrieve the stored points
                       var precision_measurement = calculatePrecision(past50);
-                      var accuracyLabel = "<a>Accuracy | "+precision_measurement+"%</a>";
+                      var accuracyLabel = "<a>Precisione | "+precision_measurement+"%</a>";
                       document.getElementById("Accuracy").innerHTML = accuracyLabel; // Show the accuracy in the nav bar.
                       swal({
-                        title: "Your accuracy measure is " + precision_measurement + "%",
+                        title: "La tua misura di precisione è " + precision_measurement + "%",
                         allowOutsideClick: false,
                         buttons: {
-                          cancel: "Recalibrate",
+                          cancel: "Ricalibrare",
                           confirm: true,
                         }
                       }).then(isConfirm => {
-                          if (isConfirm){
+                          if (isConfirm || precision_measurement > 75) {
                             //clear the calibration & hide the last middle button
                             ClearCanvas();
                             routeGame();
--- form/subjectInfo/info.js
+++ form/subjectInfo/info.js
@@ -1,17 +1,25 @@
-function save(data) {
+
+
+var saveButton = document.getElementById("submit");
+saveButton.addEventListener("click", function () {
+    download();
+    route(preGame1);
+
+});
+
+function download() {
     var data = {
         name: document.getElementById('name').value,
         email: document.getElementById('email').value,
         age: document.getElementById('age').value
     }
-    return data;
-};
-
-
-var saveButton = document.getElementById("submit");
-saveButton.addEventListener("click", function () {
-    save();
-    console.log(save());
-    route(preGame1);
-
-});
+    var CsvString = "TEST_SUBJECT_NAME,E-MAIL,AGE" + "\r\n";
+        CsvString = CsvString + data.name + ',' + data.email + ','
+            + data.age + ',' , "\r\n";
+    CsvString = "data:application/csv," + encodeURIComponent(CsvString);
+    var x = document.createElement("A");
+    x.setAttribute("href", CsvString);
+    x.setAttribute("download", data.name + " RESULTS.csv");
+    document.body.appendChild(x);
+    x.click();
+}

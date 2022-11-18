a216a24f5b2c225cab7e71eb9429a357c5c6fc7f
ponciks <mehmeteminselek@gmail.com>
2022-11-19 02:35:49 +0300

    server save

--- form/subjectInfo/info.js
+++ form/subjectInfo/info.js
@@ -1,15 +1,14 @@
-var base_url = 'https://127.0.0.1:443';
+var base_url = 'https://wafer-backend.com:443';
 const httpMethodPost = 'POST';
 const postHeaders = {
     'Accept': 'application/json',
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
 };
 
 var saveButton = document.getElementById("submit");
 saveButton.addEventListener("click", function () {
     sendToServer();
-    route(preGame1);
-
 });
 
 // function download() {
@@ -35,16 +34,23 @@ saveButton.addEventListener("click", function () {
 async function sendToServer() {
     var data = {
         name: document.getElementById('name').value,
-        phone: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
         age: document.getElementById('age').value,
         ref : document.getElementById('ref').value,
         edu : document.getElementById('edu').value,
 
     }
+    document.getElementById('bodyContainer').style.display = 'none';
+    document.getElementById('loader').style.display = 'block';
 
     await fetch(base_url + '/user/save', {
         method: httpMethodPost,
         headers: postHeaders,
         body: JSON.stringify(data)
-    }).catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+    .finally(() => {
+                document.getElementById('loader').style.display = "none";
+                route(preGame1);
+    });
 }

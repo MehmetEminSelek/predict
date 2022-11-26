fca812dca26812a06ba62c298a9c85a351230e84
ponciks <mehmeteminselek@gmail.com>
2022-11-26 05:39:00 +0300

    popup adjusment

--- route/route.js
+++ route/route.js
@@ -9,7 +9,7 @@ var webgazer_url = "/WebGazer/www/calibration.html";
 var welcome = "/form/welcome/index.html";
 var game_form = "/form/index.html";
 
- //var predict_url = "http://127.0.0.1:5500"
+//var predict_url = "http://127.0.0.1:5500"
 var predict_url = "https://wafer-experiment.com"
 
 //var game_url = "http://127.0.0.1:5502"
@@ -22,10 +22,8 @@ function route(path) {
 
 }
 
-function routeGame() {
-    
-   
-    window.open(predict_url + welcome, predict_url + welcome, "width=300,height=300,Left=-750,Top=1050");
+function routeGame() {  
+    window.open(predict_url + welcome, predict_url + welcome, "width=700,height=600,Left=-750,Top=1050");
     location.href = game_url + game_form;
 }
 
